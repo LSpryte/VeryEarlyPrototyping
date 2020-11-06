b461e6e279a33638e95b79816e4922028e5826c5
Laura <LauraSpreitzer737@gmail.com>
2020-11-06 18:53:53 -0500

    Arya Level Setup
    Made a separate scene for Arya

---
+++ EarlyPrototyping/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public int health = 10;
+    bool canTakeDamage = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (canTakeDamage)
+        {
+            Debug.Log("hit an enemy!!");
+            canTakeDamage = false;
+            StartCoroutine(NoDoubleHit());
+        }
+
+    }
+
+    //prevents weapon collider attack animation 
+    //from dealing damage more than once an attack
+    IEnumerator NoDoubleHit()
+    {
+        for (int i = 0; i < 3; i++)  //wait 3 frames
+        {
+            yield return null;
+        } 
+        canTakeDamage = true; //then can take damage again
+    }
+}
